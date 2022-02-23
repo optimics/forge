c69216b11e0c8b37213ab2c5d7a9ce16675f8ca5
Pavel Žák <pavel@zak.global>
2022-02-23 12:27:39 +0100

    feat: require block spacing

--- packages/eslint-preset/index.js
+++ packages/eslint-preset/index.js
@@ -5,6 +5,7 @@ module.exports = {
     'array-callback-return': ['error'],
     'arrow-body-style': ['error', 'as-needed'],
     'arrow-parens': ['error', 'as-needed'],
+    'block-spacing': ['error', 'always'],
     'camelcase': ['error', {
       allow: ['[a-zA-Z0-9]+__[a-zA-Z0-9]+'],
       properties: 'always',
